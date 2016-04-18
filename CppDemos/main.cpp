107640951adcdd5d09403eaf09d4c10662dce51c
Vector <anyetiangong@qq.com>
2016-04-18 16:19:15 +0800

    Initial Commit

---
+++ CppDemos/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CppDemos
+//
+//  Created by 李晓宁 on 16/4/18.
+//  Copyright © 2016年 Vector Li. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
