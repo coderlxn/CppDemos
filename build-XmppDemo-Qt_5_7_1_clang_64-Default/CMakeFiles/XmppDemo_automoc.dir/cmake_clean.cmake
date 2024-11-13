c2871018ea93e0ba09898452dbcef50d565a90e9
lixn <lixn@dogesoft.cn>
2024-11-13 11:07:23 +0800

    清理编译文件

--- build-XmppDemo-Qt_5_7_1_clang_64-Default/CMakeFiles/XmppDemo_automoc.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "XmppDemo_automoc.cpp"
-  "CMakeFiles/XmppDemo_automoc"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/XmppDemo_automoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
